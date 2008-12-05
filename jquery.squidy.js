fbb16c82659d3a8417906eeb7a10af65ab718e7d
Richard Hulse <richard.hulse@radionz.co.nz>
2008-12-05 20:49:43 +1300

    changed to capture mouse position every 40 mS
    This is because the animation of the top layer is run
    each 40 mS. The value can be changed when the plugin is started.

--- jquery.squidy.js
+++ jquery.squidy.js
@@ -12,11 +12,87 @@
 
 (function($) {
 
+	// for tracking the current position of the mouse 
+	var mouse_x = 0, 
+			mouse_y = 0;
+
   $.squidy = function(options) {
 
+		var opts = $.extend({}, $.squidy.defaults, options);
+
 	  $().mousemove(function(e){
-	     $('#status').html(e.pageX +', '+ e.pageY);
+			mouse_x = e.pageX;
+			mouse_y = e.pageY;
 	  });
+
+    $.periodic( run_aminations, {frequency: opts.animation_interval} );
 	}
 
+	function run_aminations(){
+		$('#status').html( mouse_x + ', ' + mouse_y );
+		return true;
+	}
+
+  $.squidy.defaults = {
+		animation_interval	: 0.04
+  };
+
 })(jQuery);
+
+/*
+ * jquery-periodic.js
+ *
+ * Adds a "periodic" function to jQuery which takes a callback and options for the frequency (in seconds) and a
+ * boolean for allowing parallel execution of the callback function (shielded from exceptions by a try..finally block.
+ * The first parameter passed to the callback is a controller object.
+ *
+ * Return falsy value from the callback function to end the periodic execution.
+ *
+ * For a callback which initiates an asynchronous process:
+ * There is a boolean in the controller object which will prevent the callback from executing while it is "true".
+ *   Be sure to reset this boolean to false when your asynchronous process is complete, or the periodic execution
+ *   won't continue.
+ *
+ * To stop the periodic execution, you can also call the "stop" method
+of the controller object, instead of returning
+ * false from the callback function.
+ *
+ */
+ jQuery.periodic = function (callback, options) {
+      callback = callback || (function() { return false; });
+
+      options = jQuery.extend({ },
+                              { frequency : 10,
+                                allowParallelExecution : false},
+                              options);
+
+      var currentlyExecuting = false;
+      var timer;
+
+      var controller = {
+         stop : function () {
+           if (timer) {
+             window.clearInterval(timer);
+             timer = null;
+           }
+         },
+         currentlyExecuting : false,
+         currentlyExecutingAsync : false
+      };
+
+      timer = window.setInterval(function() {
+         if (options.allowParallelExecution || !(controller.currentlyExecuting || controller.currentlyExecutingAsync)){
+            try {
+                 controller.currentlyExecuting = true;
+                 if (!(callback(controller))) {
+                     controller.stop();
+                 }
+             } finally {
+              controller.currentlyExecuting = false;
+            }
+         }
+      }, options.frequency * 1000);
+};
+
+
+
